d2610d592b754babfaaceaa3afa8a976425c60b9
Victor Nova <lostfreeman@gmail.com>
2021-12-16 16:40:02 -0800

    NCCL version is not important when NCCL is not used

--- tests/unit/util.py
+++ tests/unit/util.py
@@ -16,17 +16,18 @@ def bf16_required_version_check():
     TORCH_MAJOR = int(torch.__version__.split('.')[0])
     TORCH_MINOR = int(torch.__version__.split('.')[1])
 
-    if type(torch.cuda.nccl.version()) != tuple:
-        return False
-    else:
-        NCCL_MAJOR = torch.cuda.nccl.version()[0]
-        NCCL_MINOR = torch.cuda.nccl.version()[1]
+    if torch.distributed.is_nccl_available():
+        if type(torch.cuda.nccl.version()) != tuple:
+            return False
+        else:
+            NCCL_MAJOR = torch.cuda.nccl.version()[0]
+            NCCL_MINOR = torch.cuda.nccl.version()[1]
+            if not (NCCL_MAJOR > 2 or (NCCL_MAJOR == 2 and NCCL_MINOR >= 10)):
+                return False
 
     CUDA_MAJOR = int(torch_info['cuda_version'].split('.')[0])
-    if (TORCH_MAJOR > 1 or
-        (TORCH_MAJOR == 1 and TORCH_MINOR >= 10)) and (CUDA_MAJOR >= 11) and (
-            NCCL_MAJOR > 2 or
-            (NCCL_MAJOR == 2 and NCCL_MINOR >= 10)) and torch.cuda.is_bf16_supported():
+    if (TORCH_MAJOR > 1 or (TORCH_MAJOR == 1 and TORCH_MINOR >= 10)) and (
+            CUDA_MAJOR >= 11) and torch.cuda.is_bf16_supported():
         return True
     else:
         return False
